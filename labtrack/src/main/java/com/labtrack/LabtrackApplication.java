e8f885c351d2b32722fc465e22fde0359af7e400
Moraa <pavelmora21@gamil.com>
2026-07-09 23:29:01 -0600

    Configurar proyecto base y base de datos LabTrack

---
+++ labtrack/src/main/java/com/labtrack/LabtrackApplication.java
@@ -0,0 +1,13 @@
+package com.labtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabtrackApplication.class, args);
+	}
+
+}
